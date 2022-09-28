30735e5f55434b6c269fb72f2d6d395b700913ee
Aninga Abraham <aaninga05@gmail.com>
2022-09-28 22:16:47 +0000

    updated README.md and hot fix PEP8 code style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,16 +4,16 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    op_sign = {"+" : add, "-": sub, "*": mul, "/" : div}
+    op_sign = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(op_sign.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+a = int(sys.argv[1])
+b = int(sys.argv[3])
 
-    print("{} {} {} = {}".format(a, sys.argv[2], b, op_sign[sys.argv[2]](a, b)))
+print("{} {} {} = {}".format(a, sys.argv[2], b, op_sign[sys.argv[2]](a, b)))
