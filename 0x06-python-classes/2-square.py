41e5ab5d18e7f8423c4088376abe736b80e9bbb1
Aninga Abraham <aaninga05@gmail.com>
2022-10-12 22:17:43 +0000

    hot fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,4 +15,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must >= 0")
-        self.__name = size
+        self.__size = size
