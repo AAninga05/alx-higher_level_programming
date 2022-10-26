a0e5b1164ed652dc07a8e770bbea6aa10c417679
Aninga Abraham <aaninga05@gmail.com>
2022-10-26 22:07:50 +0000

    advance task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Defines a class Mylist"""
 
 class MyList(list):
-
+    """Defines a class my list"""
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
